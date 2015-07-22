49a7d4e27aa4c1965f7747c3c5479a1b63b1b062
Junchao Zhang <junchaozhang@jc-mbp.na.tibco.com>
2015-07-22 11:27:19 -0700

    user authorization

--- public/ng/controllers/mainController.js
+++ public/ng/controllers/mainController.js
@@ -1,5 +1,5 @@
 angular.module('spicyTaste')
-    .controller('MainController', function($rootScope, $location, $mdDialog, $http, SessionService, UserService, CONSTANTS) {
+    .controller('MainController', function($rootScope, $scope, $location, $mdDialog, $http, SessionService, UserService, CONSTANTS) {
         var vm = this;
         init();
 
@@ -7,82 +7,95 @@ angular.module('spicyTaste')
             vm.showMobileMenu = !vm.showMobileMenu;
         }
 
-        vm.showLoginDialog = function(ev) {
-            $mdDialog.show({
+        $scope.showLoginDialog = function(ev, postLogin, title) {
+            var promise = $mdDialog.show({
                 clickOutsideToClose: true,
                 templateUrl: 'ng/views/dialogs/login.html',
                 targetEvent: ev,
-                controller: 'MainController',
-                controllerAs: 'main'
-            }).then(function(user) {
-                $rootScope.currentUser = user;
+                controller: loginController,
+                controllerAs: 'login',
+                locals: {
+                    dialogTitle: title
+                }
             });
+            if (postLogin) {
+                return promise;
+            } else {
+                promise.then(function(user) {
+                    $rootScope.currentUser = user;
+                    $location.path('/');
+                });
+            }
         };
 
-        vm.fbLogin = function() {
-            FB.login(function(response) {
-                if (response.status === 'connected') {
-                    // Logged into your app and Facebook.
-                    FB.api('/me', function(response) {
-                        var fbUser = {
-                            userName: response.name,
-                            email: response.email,
-                            password: CONSTANTS.SOCIAL_PASS,
-                            photoUrl: 'http://graph.facebook.com/' + response.id + '/picture?type=large',
-                            linkedSocial: CONSTANTS.FACEBOOK
-                        }
-                        UserService.socialLogin(fbUser).then(function(user) {
-                            user.loginType = CONSTANTS.FACEBOOK;
-                            afterAuth(user);
-                        })
+        function loginController(dialogTitle, $mdDialog, UserService) {
+            var dvm = this;
+            dvm.title = dialogTitle;
+            dvm.email = "";
+            dvm.password = "";
+            dvm.userName = "";
 
-                    });
-                } else if (response.status === 'not_authorized') {
-                    // The person is logged into Facebook, but not your app.
-                } else {
-                    // The person is not logged into Facebook, so we're not sure if
-                    // they are logged into this app or not.
-                }
-            }, {
-                scope: 'public_profile,email'
-            });
-        }
+            dvm.fbLogin = function() {
+                FB.login(function(response) {
+                    if (response.status === 'connected') {
+                        // Logged into your app and Facebook.
+                        FB.api('/me', function(response) {
+                            var fbUser = {
+                                userName: response.name,
+                                email: response.email,
+                                password: CONSTANTS.SOCIAL_PASS,
+                                photoUrl: 'http://graph.facebook.com/' + response.id + '/picture?type=large',
+                                linkedSocial: CONSTANTS.FACEBOOK
+                            }
+                            UserService.socialLogin(fbUser).then(function(user) {
+                                user.loginType = CONSTANTS.FACEBOOK;
+                                afterAuth(user);
+                            })
 
-        vm.login = function() {
-            UserService.login(vm.email, vm.password).then(function(user) {
-                user.loginType = CONSTANTS.EMAIL;
-                afterAuth(user);
-            });
-        }
+                        });
+                    } else if (response.status === 'not_authorized') {
+                        // The person is logged into Facebook, but not your app.
+                    } else {
+                        // The person is not logged into Facebook, so we're not sure if
+                        // they are logged into this app or not.
+                    }
+                }, {
+                    scope: 'public_profile,email'
+                });
+            }
 
-        vm.signUp = function() {
-            var newUser = {
-                userName: vm.userName,
-                email: vm.email,
-                password: vm.password,
-                photoUrl: 'http://www.gravatar.com/avatar/' + md5.createHash(vm.email),
-                linkedSocial: CONSTANTS.EMAIL
-            };
+            dvm.login = function() {
+                UserService.login(dvm.email, dvm.password).then(function(user) {
+                    user.loginType = CONSTANTS.EMAIL;
+                    afterAuth(user);
+                });
+            }
 
-            UserService.create(newUser).then(function(user) {
-                user.loginType = CONSTANTS.EMAIL;
-                afterAuth(user);
-            });
+            dvm.signUp = function() {
+                var newUser = {
+                    userName: dvm.userName,
+                    email: dvm.email,
+                    password: dvm.password,
+                    photoUrl: 'http://www.gravatar.com/avatar/' + md5.createHash(dvm.email),
+                    linkedSocial: CONSTANTS.EMAIL
+                };
 
-        }
+                UserService.create(newUser).then(function(user) {
+                    user.loginType = CONSTANTS.EMAIL;
+                    afterAuth(user);
+                });
 
-        function afterAuth(user) {
-            var token = $http.defaults.headers.common['X-Auth'];
-            SessionService.setLocal(CONSTANTS.LOCAL_STORAGE_KEY, token);
-            $mdDialog.hide(user);
+            }
+
+            function afterAuth(user) {
+                var token = $http.defaults.headers.common['X-Auth'];
+                SessionService.setLocal(CONSTANTS.LOCAL_STORAGE_KEY, token);
+                $mdDialog.hide(user);
+            }
         }
 
         function init() {
             vm.showMobileMenu = false;
-            vm.email = "";
-            vm.password = "";
-            vm.userName = "";
-
             var loginedToken = SessionService.getLocal(CONSTANTS.LOCAL_STORAGE_KEY);
             if (loginedToken) {
                 $http.defaults.headers.common['X-Auth'] = loginedToken;
