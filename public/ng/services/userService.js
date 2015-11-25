3aca02952c03a9318e1d1795dab5695e9e34827e
Junchao Zhang <junchaozhang@JC-MBP.local>
2015-11-24 21:30:48 -0800

    revise FB services

--- public/ng/services/userService.js
+++ public/ng/services/userService.js
@@ -1,13 +1,79 @@
 angular.module('spicyTaste')
-    .factory('UserService', function($http, $rootScope, $window, CONSTANTS) {
+    .factory('UserService', function($http, $rootScope, $q, $window, CONSTANTS, SessionService) {
         'use strict';
 
         var userFactory = {};
         var baseUrl = '/api/users/';
 
+        function afterAuth(result) {
+            $http.defaults.headers.common['X-Auth'] = result.token;
+            SessionService.setLocal(CONSTANTS.LOCAL_STORAGE_KEY, result.token);
+            $rootScope.currentUser = result.user;
+            return true;
+        }
+
+        function authFB(fbUser) {
+
+            return $http.post('/api/auth/facebook', fbUser).then(function(response) {
+                return afterAuth(response.data);
+            }, function() {});
+        }
+
         //fb login
-        userFactory.fbLogin = function(fbUser) {
-            return $http.post('/api/auth/facebook', fbUser);
+        userFactory.fbLogin = function() {
+            FB.login(function(response) {
+                if (response.status === 'connected') {
+
+                } else if (response.status === 'not_authorized') {
+                    // The person is logged into Facebook, but not your app.
+                } else {
+                    // The person is not logged into Facebook, so we're not sure if
+                    // they are logged into this app or not.
+                }
+            }, {
+                scope: 'public_profile,email'
+            });
+        };
+
+        userFactory.watchFBAuthChange = function() {
+            FB.Event.subscribe('auth.authResponseChange', function(res) {
+                if (res.status === 'connected') {
+                    userFactory.getFBUserInfo().then(function(fbUser) {
+                        if (!$rootScope.currentUser) {
+                            authFB(fbUser);
+                        } else {
+                            userFactory.updateInfo($rootScope.currentUser._id, 'facebook', fbUser.facebook);
+                        }
+                    });
+                } else {}
+
+            });
+        };
+
+        userFactory.getFBUserInfo = function() {
+            var deffered = $q.defer();
+
+            FB.api('/me', {
+                fields: 'id, name, email'
+            }, function(response) {
+                if (!response || response.error) {
+                    deffered.reject('FB api/me error');
+                }
+
+                var fbUser = {
+                    userName: response.name,
+                    email: response.email,
+                    photoUrl: 'http://graph.facebook.com/' + response.id + '/picture?type=large',
+                    facebook: {
+                        id: response.id,
+                        email: response.email
+                    }
+                };
+                deffered.resolve(fbUser);
+
+            });
+
+            return deffered.promise;
         };
 
         //login user
@@ -16,13 +82,31 @@ angular.module('spicyTaste')
             return $http.post('/api/auth', {
                 email: email,
                 password: password
+            }).then(function(response) {
+                return afterAuth(response.data);
+            }, function() {
+                return false;
             });
         };
 
         //logout user
         userFactory.logout = function() {
+            var deffered = $q.defer();
             delete $http.defaults.headers.common['X-Auth'];
             $window.localStorage.removeItem(CONSTANTS.LOCAL_STORAGE_KEY);
+
+            FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                    FB.logout(function() {
+                        deffered.resolve();
+                    });
+                } else {
+                    deffered.resolve();
+                }
+            });
+
+            $rootScope.currentUser = null;
+            return deffered.promise;
         };
 
         //search user by field
@@ -56,8 +140,13 @@ angular.module('spicyTaste')
 
         //update user
         userFactory.update = function(user) {
-            return $http.put(baseUrl + user.userId, user).then(function(response) {
-                return response.data;
+            return $http.put(baseUrl + user.userId, user);
+        };
+
+        userFactory.updateInfo = function(userId, field, newValue) {
+            return $http.put(baseUrl + userId + '/field', {
+                field: field,
+                newValue: newValue
             });
         };
 
@@ -68,7 +157,6 @@ angular.module('spicyTaste')
 
         //authorize user
         userFactory.authorize = function(requirePermissions) {
-            console.log('requirePermissions', requirePermissions);
             return userFactory.getCurrentUser().success(function(user) {
                 if (user && user.role === 'ADMIN') {
                     return true;
